e35a1e4068619f4fb924cbff0897f833cbdf5afc
ta2ye0n <tyeon42051@naver.com>
2024-09-25 20:50:23 +0900

    init

---
+++ src/main/java/com/mango/amango/AMangoApplication.java
@@ -0,0 +1,13 @@
+package com.mango.amango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AMangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mango/amango/AMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.amango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AMangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
