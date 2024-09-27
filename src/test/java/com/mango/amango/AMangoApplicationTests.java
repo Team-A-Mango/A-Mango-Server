78796d840f841ab3fb7984064ca6b6e978a03922
ta2ye0n <tyeon42051@naver.com>
2024-09-27 12:17:14 +0900

    refactor :: 주석처리

--- src/test/java/com/mango/amango/AMangoApplicationTests.java
+++ src/test/java/com/mango/amango/AMangoApplicationTests.java
@@ -3,7 +3,7 @@ package com.mango.amango;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AMangoApplicationTests {
 
     @Test
