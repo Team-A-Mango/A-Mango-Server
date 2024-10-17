eab50678bf04c90f91ffedf1f8a247c1b16d0d69
이현준 <11dlguswns@naver.com>
2024-10-17 20:17:42 +0900

    jwtProperties 구현

--- src/main/java/com/mango/amango/global/security/jwt/service/JwtService.java
+++ src/main/java/com/mango/amango/global/security/jwt/service/JwtService.java
@@ -9,11 +9,12 @@ import com.mango.amango.global.exception.CustomException;
 import com.mango.amango.global.security.CustomUserDetailService;
 import com.mango.amango.global.security.jwt.JwtRule;
 import com.mango.amango.global.security.jwt.TokenStatus;
+import com.mango.amango.global.security.jwt.properties.JwtProperties;
 import com.mango.amango.global.security.jwt.util.JwtGenerator;
 import com.mango.amango.global.security.jwt.util.JwtUtil;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,36 +27,26 @@ import static java.time.LocalDateTime.*;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class JwtService {
 
     private final CustomUserDetailService customUserDetailService;
     private final RefreshTokenRepository refreshTokenRepository;
-
-    private final Key ACCESS_SECRET_KEY;
-    private final Key REFRESH_SECRET_KEY;
-    private final long ACCESS_EXPIRATION;
-    private final long REFRESH_EXPIRATION;
-
-    public JwtService(CustomUserDetailService customUserDetailService, RefreshTokenRepository refreshTokenRepository,
-                      @Value("${jwt.access-secret}") String ACCESS_SECRET_KEY,
-                      @Value("${jwt.refresh-secret}") String  REFRESH_SECRET_KEY,
-                      @Value("${jwt.access-expiration}") long ACCESS_EXPIRATION,
-                      @Value("${jwt.refresh-expiration}") long REFRESH_EXPIRATION) {
-        this.customUserDetailService = customUserDetailService;
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.ACCESS_SECRET_KEY = JwtUtil.getSigningKey(ACCESS_SECRET_KEY);
-        this.REFRESH_SECRET_KEY = JwtUtil.getSigningKey(REFRESH_SECRET_KEY);
-        this.ACCESS_EXPIRATION = ACCESS_EXPIRATION;
-        this.REFRESH_EXPIRATION = REFRESH_EXPIRATION;
-    }
+    private final JwtProperties jwtProperties;
 
     public String generateAccessToken(User requestUser) {
-        return JwtGenerator.generateAccessToken(ACCESS_SECRET_KEY, ACCESS_EXPIRATION, requestUser);
+        return JwtGenerator.generateAccessToken(
+                jwtProperties.getAccessSecret(),
+                jwtProperties.getAccessExpiration(),
+                requestUser);
     }
 
     @Transactional
     public String generateRefreshToken(User requestUser) {
-        String refreshToken = JwtGenerator.generateRefreshToken(REFRESH_SECRET_KEY, REFRESH_EXPIRATION, requestUser);
+        String refreshToken = JwtGenerator.generateRefreshToken(
+                jwtProperties.getRefreshSecret(),
+                jwtProperties.getRefreshExpiration(),
+                requestUser);
 
         refreshTokenRepository.save(new RefreshToken(requestUser.getNickname(), refreshToken));
         return refreshToken;
@@ -65,8 +56,8 @@ public class JwtService {
         return TokenRes.builder()
                 .accessToken(generateAccessToken(requestUser))
                 .refreshToken(generateRefreshToken(requestUser))
-                .accessTokenExpiresIn(now().plusSeconds(ACCESS_EXPIRATION / 1000))
-                .refreshTokenExpiresIn(now().plusSeconds(REFRESH_EXPIRATION / 1000))
+                .accessTokenExpiresIn(now().plusSeconds(jwtProperties.getAccessExpiration() / 1000))
+                .refreshTokenExpiresIn(now().plusSeconds(jwtProperties.getRefreshExpiration() / 1000))
                 .build();
     }
 
@@ -75,21 +66,25 @@ public class JwtService {
     }
 
     public boolean validateAccessToken(String token) {
-        return JwtUtil.getTokenStatus(token, ACCESS_SECRET_KEY) == TokenStatus.AUTHENTICATED;
+        return JwtUtil.getTokenStatus(token, jwtProperties.getAccessSecret()) == TokenStatus.AUTHENTICATED;
     }
 
     public boolean validateRefreshToken(String token, String username) {
-        boolean isRefreshValid = JwtUtil.getTokenStatus(token, REFRESH_SECRET_KEY) == TokenStatus.AUTHENTICATED;
+        boolean isRefreshValid = JwtUtil.getTokenStatus(
+                token, jwtProperties.getRefreshSecret()) == TokenStatus.AUTHENTICATED;
 
         RefreshToken savedRefreshToken = refreshTokenRepository.findByUsername(username)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.REFRESH_TOKEN_NOT_FOUND));
+
         boolean isRefreshTokenMatch = savedRefreshToken.getToken().equals(token);
 
         return isRefreshValid && isRefreshTokenMatch;
     }
 
     public Authentication getAuthentication(String token) {
-        UserDetails principal = customUserDetailService.loadUserByUsername(getUserId(token, ACCESS_SECRET_KEY));
+        UserDetails principal = customUserDetailService.loadUserByUsername(
+                getUserId(token, jwtProperties.getAccessSecret()));
+
         return new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
     }
 
@@ -105,13 +100,14 @@ public class JwtService {
     public void logout(User requestUser) {
         RefreshToken refreshToken = refreshTokenRepository.findByUsername(requestUser.getNickname())
                 .orElseThrow(() -> new CustomException(CustomErrorCode.REFRESH_TOKEN_NOT_FOUND));
+
         refreshTokenRepository.delete(refreshToken);
     }
 
     public String getUsernameFromRefresh(String refreshToken) {
         try {
             return Jwts.parserBuilder()
-                    .setSigningKey(REFRESH_SECRET_KEY)
+                    .setSigningKey(jwtProperties.getRefreshSecret())
                     .build()
                     .parseClaimsJws(refreshToken)
                     .getBody()
