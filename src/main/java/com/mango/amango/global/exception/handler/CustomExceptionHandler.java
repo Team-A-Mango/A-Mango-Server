f554b63147568e70fd418a06e82a9593de278bfb
이현준 <11dlguswns@naver.com>
2024-10-17 16:34:05 +0900

    Validation 예외 처리 핸들러 추가

--- src/main/java/com/mango/amango/global/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/mango/amango/global/exception/handler/CustomExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.mango.amango.global.exception.handler;
 
+import com.mango.amango.global.exception.CustomErrorCode;
 import com.mango.amango.global.exception.CustomException;
 import com.mango.amango.global.exception.CustomErrorRes;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,5 +33,22 @@ public class CustomExceptionHandler {
                 .build();
 
         return ResponseEntity.status(statusCode).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomErrorRes> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        CustomErrorRes response = CustomErrorRes.builder()
+                .status(CustomErrorCode.VALIDATION_FAILED)
+                .statusMessage(getErrorMessage(e))
+                .build();
+
+        return ResponseEntity.status(BAD_REQUEST).body(response);
+    }
+
+    private String getErrorMessage(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("유효성 검사에 실패했습니다.");
     }
 }
