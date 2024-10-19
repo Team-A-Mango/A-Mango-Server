14bc70d6ef571cbcc253dd2ad9553e89d03184b9
이현준 <11dlguswns@naver.com>
2024-10-19 14:54:22 +0900

    로그인 학교 이메일 정규식 제한

--- src/main/java/com/mango/amango/domain/auth/entity/dto/request/LoginReq.java
+++ src/main/java/com/mango/amango/domain/auth/entity/dto/request/LoginReq.java
@@ -1,6 +1,7 @@
 package com.mango.amango.domain.auth.entity.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class LoginReq {
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@gsm.hs.kr$", message = "gsm.hs.kr 도메인을 사용하는 메일이 아닙니다.")
     private final String email;
     @NotBlank
     private final String password;
