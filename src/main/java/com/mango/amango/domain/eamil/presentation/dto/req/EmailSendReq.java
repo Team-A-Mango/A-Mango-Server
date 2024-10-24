1aea504be305402f437937f0d47f6bc238405f35
ta2ye0n <tyeon42051@naver.com>
2024-10-24 15:10:39 +0900

    refactor :: record class

--- src/main/java/com/mango/amango/domain/eamil/presentation/dto/req/EmailSendReq.java
+++ src/main/java/com/mango/amango/domain/eamil/presentation/dto/req/EmailSendReq.java
@@ -1,14 +1,9 @@
 package com.mango.amango.domain.eamil.presentation.dto.req;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class EmailSendReq {
-    private String email;
+public record EmailSendReq (
+        String email
+){
 }
