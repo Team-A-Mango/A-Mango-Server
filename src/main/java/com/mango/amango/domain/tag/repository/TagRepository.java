85c705ddf040a128e99b3bd683251290dfb5bb34
이현준 <11dlguswns@naver.com>
2024-12-17 17:46:12 +0900

    remove :: Tag 관련 기능 삭제

--- src/main/java/com/mango/amango/domain/tag/repository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mango.amango.domain.tag.repository;
-
-import com.mango.amango.domain.tag.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
