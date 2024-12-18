7971c41a2f4e26be7e8af9226a01a76af7fabcb7
ta2ye0n <tyeon42051@naver.com>
2024-12-18 09:01:00 +0900

    delete :: mail 부분 삭제

--- src/main/java/com/mango/amango/domain/eamil/service/EmailSendService.java
+++
@@ -1,7 +0,0 @@
-package com.mango.amango.domain.eamil.service;
-
-import com.mango.amango.domain.eamil.presentation.dto.req.EmailSendReq;
-
-public interface EmailSendService {
-    void execute(EmailSendReq email);
-}
