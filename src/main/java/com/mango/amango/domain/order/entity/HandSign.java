dffa83e206350f109b62a74d1a67d7b75924e653
이현준 <11dlguswns@naver.com>
2024-12-18 11:07:15 +0900

    add :: HandSign 인덱스 값 추가

--- src/main/java/com/mango/amango/domain/order/entity/HandSign.java
+++ src/main/java/com/mango/amango/domain/order/entity/HandSign.java
@@ -1,9 +1,16 @@
 package com.mango.amango.domain.order.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum HandSign {
-    NULL,
-    CALL,
-    ROCK,
-    PEACE,
-    OK
+    NULL(0),
+    CALL(1),
+    ROCK(2),
+    PEACE(3),
+    OK(4);
+
+    private final int value;
 }
