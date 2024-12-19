d8ea38cdeb7728f3ed1fe80f564f8f80509bac2c
이현준 <11dlguswns@naver.com>
2024-12-19 11:03:19 +0900

    fix :: 수신자 발신자 변경

--- src/main/java/com/mango/amango/global/sms/event/SendMessageEvent.java
+++ src/main/java/com/mango/amango/global/sms/event/SendMessageEvent.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class SendEmailEvent {
+public class SendMessageEvent {
     private final String fromPhoneNumber;
     private final String message;
 }
