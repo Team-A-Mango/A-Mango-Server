bf2e9b6a3db064b883c0272d20cfafc3e97dedef
ta2ye0n <tyeon42051@naver.com>
2024-12-19 17:40:58 +0900

    add :: ProductLikeRepository

--- src/main/java/com/mango/amango/domain/product/repository/ProductLikeRepository.java
+++ src/main/java/com/mango/amango/domain/product/repository/ProductLikeRepository.java
@@ -3,9 +3,11 @@ package com.mango.amango.domain.product.repository;
 import com.mango.amango.domain.product.entity.ProductLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
     ProductLike findByProductIdAndUserId(Long productId, UUID userId);
+
+    List<ProductLike> findByUserId(UUID userId);
 }
