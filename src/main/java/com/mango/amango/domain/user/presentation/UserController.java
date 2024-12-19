e1894297c855ed95a183a5bf142649804722f08d
ta2ye0n <tyeon42051@naver.com>
2024-12-19 19:59:28 +0900

    add :: endpoint

--- src/main/java/com/mango/amango/domain/user/presentation/UserController.java
+++ src/main/java/com/mango/amango/domain/user/presentation/UserController.java
@@ -1,6 +1,8 @@
 package com.mango.amango.domain.user.presentation;
 
+import com.mango.amango.domain.user.presentation.dto.response.GetMyInfoRes;
 import com.mango.amango.domain.user.presentation.dto.response.GetMyOrdersRes;
+import com.mango.amango.domain.user.service.GetMyInfoService;
 import com.mango.amango.domain.user.service.GetMyOrdersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final GetMyOrdersService getMyOrdersService;
+    private final GetMyInfoService getMyInfoService;
 
     @GetMapping
     public ResponseEntity<GetMyOrdersRes> getMyOrders() {
         GetMyOrdersRes res = getMyOrdersService.execute();
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<GetMyInfoRes> getMyOrdersInfo() {
+        GetMyInfoRes res = getMyInfoService.execute();
         return ResponseEntity.ok(res);
     }
 }
