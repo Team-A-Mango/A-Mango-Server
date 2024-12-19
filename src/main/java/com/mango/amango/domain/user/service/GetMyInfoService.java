defbc8b3c6051a7d44170525c8af102229285f1b
ta2ye0n <tyeon42051@naver.com>
2024-12-19 19:59:18 +0900

    add :: GetMyInfoService

---
+++ src/main/java/com/mango/amango/domain/user/service/GetMyInfoService.java
@@ -0,0 +1,7 @@
+package com.mango.amango.domain.user.service;
+
+import com.mango.amango.domain.user.presentation.dto.response.GetMyInfoRes;
+
+public interface GetMyInfoService {
+    GetMyInfoRes execute();
+}
